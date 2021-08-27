64f80c19beab5e6d0f18620fafd4ab7ab7d0f1e0
Sanskar Bajpai <sanskarbajpai2907@gmail.com>
2021-08-27 20:17:51 +0530

    Server: Added a basic NodeJS server to the application.

---
+++ Server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const requestListener = function (req, res) {
+    res.writeHead(200);
+    res.end('Server 1');
+}
+
+const server = http.createServer(requestListener);
+server.listen(8888);
